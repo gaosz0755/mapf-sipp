6de6e48259f5bb896f789c94b1ef95a9ac82749a
Fitz Laddaran <fladdara@sfu.ca>
2022-08-07 14:12:17 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from instance.import_map import import_map
+import argparse
+import glob
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description = "Multi-Agent Pathfinding using Safe-Interval Path Finding Algorithms")
+    
+    parser.add_argument("--instance", type = str, default = None, help = "File name of instance(s)")
+    
+    args = parser.parse_args()
+    
+    for f in sorted(glob.glob(args.instance)):
+        
+        my_map = import_map(f)
+        
+        
+
+
+
+
+
+
