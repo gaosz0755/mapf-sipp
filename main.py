0ec089d654b69af852e56ae125694e270c284b3c
Fitz Laddaran <fladdara@sfu.ca>
2022-08-08 13:47:28 -0700

    added arg options

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ import glob
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description = "Multi-Agent Pathfinding using Safe-Interval Path Finding Algorithms")
-    
     parser.add_argument("--instance", type = str, default = None, help = "File name of instance(s)")
-    
+    parser.add_argument("--alg", type = str, default = None, help = "Name of algorithm to use")
     args = parser.parse_args()
     
     for f in sorted(glob.glob(args.instance)):
-        
         my_map = import_map(f)
         
         
