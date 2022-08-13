343058d0bd3aad28c7c6ae7d8e637cff02f08edd
Fitz Laddaran <fladdara@sfu.ca>
2022-08-13 14:30:11 -0700

    initial commit

---
+++ solver/solver.py
@@ -0,0 +1,12 @@
+class Solver:
+
+    def __init__(self, my_map, agents, search_alg):
+        self.map = my_map
+        self.agents = agents
+        self.search_alg = search.alg
+        
+    def find_solution(self):
+        paths = []
+        
+        
+
