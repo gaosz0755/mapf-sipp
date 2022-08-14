16d63a50a730c67b28edefa78b0099791bb1c34c
Fitz Laddaran <fladdara@sfu.ca>
2022-08-13 19:46:59 -0700

    added comments

--- instance/import_instance.py
+++ instance/import_instance.py
@@ -1,9 +1,19 @@
+#####################################################################
+#                                                                   #
+# Imports instance to be used in the experiment.                    #
+#                                                                   #
+#####################################################################
+
+
 from objects.safe_intervals import Safe_interval
 from objects.map import Map
 from pathlib import Path
 
+### function: reads-in the details from "filename" to be used in the experiment
+### return: the map; agent(s) details
 def import_instance(filename):
 
+    ### opening the file
     try:
         with open(filename, "r") as f:
             rows = int(f.readline().split()[1])
@@ -13,18 +23,20 @@ def import_instance(filename):
         print("File" + no_file_found.filename + "is not found.") 
     
     f.readline()
-    loc = [[for _ in range(rows)] for _ in range(cols)]
+    loc = [[[] for _ in range(rows)] for _ in range(cols)]                                    # 2d-list, where every element is a list
     
+    ### identifying locations of static obstacles within the map
     for i in range(rows):
         line = f.readline()
         for j in range(cols):
             if line[j] == ".": loc[i][j].append(Safe_interval())
             elif line[j] != "@": raise ValueError("Error while parsing environment: unexpected symbol at '{line[j]}'.")
             
-    my_map = Map(rows, cols, loc)
-    f.readline()
+    my_map = Map(rows, cols, loc)                                                             # creating map
     agents = []
+    f.readline()
     
+    ### identifying start/goal locations of agents 
     for k in range(n_agents):
         line = f.readline()
         s_x, s_y, g_x, g_y = [int(x) for x in line.split(' ')]
