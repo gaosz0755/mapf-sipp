5193f19e02a3400dcbf153cf05fe23cf1a206ae1
Fitz Laddaran <fladdara@sfu.ca>
2022-08-15 15:34:59 -0700

    implemented

--- algorithms/a_star.py
+++ algorithms/a_star.py
@@ -1,4 +1,4 @@
-from .Search_alg import Search_alg
+from .search_alg import Search_alg
 from objects.open_list import Open_list
 from objects.closed_list import Closed_list
 from objects.node import Node
@@ -9,7 +9,7 @@ class A_star(Search_alg):
         
         open_list = Open_list()
         closed_list = Closed_list()
-        root = Node(s_x, s_y, h_val = self.heuristic(s_x, s_y, g_x, g_y))        
+        root = Node_astar(s_x, s_y, h_val = self.heuristic(s_x, s_y, g_x, g_y))        
         open_list.push_node(root)
         
         while open_list.__len__ > 0:
@@ -19,9 +19,9 @@ class A_star(Search_alg):
                 self.solution_found()
                 return True
                 
+            for x, y in my_map.successors(curr):
+                child = Node_astar(x, y, curr.g_val + 1, self.heuristic(x, y, g_x, g_y), curr)
+                if closed_list.expanded(child): continue
+                open_list.push_node(child)
             
-                for neighbour in my_map.get_neighbours(
-            
-            
-            
-
+        return False    
--- algorithms/search_alg.py
+++ algorithms/search_alg.py
@@ -3,16 +3,20 @@ class Search_alg:
     ### function: constructor
     def __init__(self, heuristic):
         self.heuristic = heuristic
+        self.goal_node = None
         self.goal_found = False
     
     
     def find_path(self, my_map, s_x, s_y, g_x, g_y):
-        return search(my_map, s_x, s_y, g_x, g_y)
+        self.search(my_map, s_x, s_y, g_x, g_y)
+        if self.goal_node is not None: self.goal_found = True
+        return self.goal_found, self.goal_node
     
     def search(self, my_map, s_x, s_y, g_x, g_y):
         raise NotImplementedError
     
-    def solution_found(self):
+    def solution_found(self, goal_node):
+        self.goal_node = goal_node
         self.goal_found = True
     
     @staticmethod
