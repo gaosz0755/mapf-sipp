71bfd701236bb87bd36e9d531254992430ada31a
Fitz Laddaran <fladdara@sfu.ca>
2022-08-18 18:35:58 -0700

    added priorities

--- objects/agent.py
+++ objects/agent.py
@@ -1,7 +1,8 @@
 class Agent:
 
-    def __init__(self, s_x, s_y, g_x, g_y): 
+    def __init__(self, s_x, s_y, g_x, g_y, priority): 
         self.s_x = s_x
         self.s_y = s_y
         self.g_x = g_x
         self.g_y = g_y
+        self.priority = priority
