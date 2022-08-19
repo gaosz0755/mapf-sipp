8c688206f97bc88b0c4ed8887ee1702ca938088e
Fitz Laddaran <fladdara@sfu.ca>
2022-08-18 17:19:08 -0700

    added back multiple instances function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from instance.animate import animate
 from instance.import_instance import import_instance, import_instance_astar
 from solver.solver import Solver
 import argparse
+import glob 
 
 if __name__ == '__main__':
     
@@ -10,14 +11,16 @@ if __name__ == '__main__':
     parser.add_argument("--instance", type = str, default = None, help = "File name of instance(s)")
     parser.add_argument("--alg", type = str, default = None, help = "Name of algorithm to use")
     args = parser.parse_args()
+    
+    for f in sorted(glob.glob(args.instance)):
+    
+        if args.alg == "astar":
+            my_map, agents = import_instance_astar(f)
+            print("Running: A* algorithm")
+            paths = Solver(my_map, agents, A_star()).find_solution()
+        else:
+            my_map, agents = import_instance(f)
+            if args.alg == "asipp":
+                print("Running: Anytime Safe-Interval Path Planning")
 
-    if args.alg == "astar":
-        my_map, agents = import_instance_astar(f)
-        print("Running: A* algorithm")
-        paths = Solver(my_map, agents, A_star()).find_solution()
-    else:
-        my_map, agents = import_instance(f)
-        if args.alg == "asipp":
-            print("Running: Anytime Safe-Interval Path Planning")
-
-    animate(my_map, paths, [])
+        animate(my_map, paths, [])
